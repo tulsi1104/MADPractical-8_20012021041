544626e5115278fe12bcab36fe5ff7bb6d44680a
tulsi1104 <tulsiii1104@gmail.com>
2022-10-04 12:36:41 +0530

    Initial committed

---
+++ app/src/main/java/com/example/madpractical_8_20012021041/SplashActivity.kt
@@ -0,0 +1,50 @@
+package com.example.madpractical_8_20012021041
+
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.widget.ImageView
+import com.google.android.material.animation.AnimationUtils
+
+class SplashActivity : AppCompatActivity(),Animation.AnimationListener {
+    lateinit var guni_imgg:ImageView
+    lateinit var logo_framebyframeanimation:AnimationDrawable
+    lateinit var twinanimation:Animation
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        guni_imgg=findViewById(R.id.guni_img)
+        guni_imgg.setBackgroundResource(R.drawable.uvpce_logo_list)
+        logo_framebyframeanimation=guni_imgg.background as AnimationDrawable
+        twinanimation=android.view.animation.AnimationUtils.loadAnimation(this,R.anim.twin_animation)
+        twinanimation.setAnimationListener(this)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if(hasFocus){
+            logo_framebyframeanimation.start()
+            guni_imgg.startAnimation(twinanimation)
+        }
+        else
+        {
+            logo_framebyframeanimation.stop()
+        }
+    }
+
+    override fun onAnimationStart(p0: Animation?) {
+
+    }
+
+    override fun onAnimationEnd(p0: Animation?) {
+      Intent(this,MainActivity::class.java).also { startActivity(it)}
+        overridePendingTransition(R.anim.scale_center_in,R.anim.scale_center_out)
+        finish()
+    }
+
+    override fun onAnimationRepeat(p0: Animation?) {
+
+    }
+}
